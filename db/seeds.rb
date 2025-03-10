bda3c624f9465f3e49713e6cc80b198e1e01daeb
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-10 23:55:44 +0900

    first seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.development?
+  unless User.exists?
+    user = User.create!
+    user.create_user_credential!(email: 'example@example.com', password: 'P@ssW0rd', password_confirmation: 'P@ssW0rd')
+    user.create_user_profile!(name: 'First example user', lang: I18n.default_locale)
+
+    user.posts.create!(content: 'Hello!')
+    user.posts.create!(content: 'World!')
+    user.posts.create!(content: 'foo!')
+    user.posts.create!(content: 'bar!!')
+    user.posts.create!(content: 'baz!!!')
+    user.posts.create!(content: 'awesome!!!!')
+  end
+end
