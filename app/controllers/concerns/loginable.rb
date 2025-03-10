20e06ca771fe4f454dd3a97fee911a05a3317be8
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-10 12:58:44 +0900

    Hypothetically define how to determine login status

---
+++ app/controllers/concerns/loginable.rb
@@ -0,0 +1,13 @@
+module Loginable
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :current_user, :logged_in?
+  end
+
+  private
+
+  def logged_in? = current_user.present?
+
+  def current_user = nil # TODO: need to implement it
+end
