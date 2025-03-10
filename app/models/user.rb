124838ca03d403501d4a9ee3cb513aa970314e16
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-11 00:35:57 +0900

    User#lang delegate to UserProfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_one :user_profile
   has_one :user_registration
 
-  delegate :name, to: :user_profile, allow_nil: true
+  delegate :name, :lang, to: :user_profile, allow_nil: true
 end
