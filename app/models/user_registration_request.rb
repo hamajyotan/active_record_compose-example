4b40a9069b9c7cff6de5f97b8d76594a50859647
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-10 15:54:28 +0900

    Already registered e-mail addresses are not allowed to register.

--- app/models/user_registration_request.rb
+++ app/models/user_registration_request.rb
@@ -11,11 +11,19 @@ class UserRegistrationRequest < ActiveRecordCompose::Model
 
   after_commit :send_email_notification
 
+  validate :unique_email
+
   private
 
   attr_reader :user_registration
 
   def send_email_notification
     UserRegistrationMailer.with(user_registration:).requested.deliver_later
+  end
+
+  def unique_email
+    return unless UserCredential.exists?(email:)
+
+    errors.add(:email, :taken)
   end
 end
