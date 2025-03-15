b05860aaf51d2ab685b848be868a98118c3f498f
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-15 22:09:46 +0900

    add mailer test

--- test/mailers/previews/user_registration_mailer_preview.rb
+++ test/mailers/previews/user_registration_mailer_preview.rb
@@ -1,3 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_registration_mailer
 class UserRegistrationMailerPreview < ActionMailer::Preview
+  def requested
+    user_registration = UserRegistration.new(id: -1, email: "test@example.com")
+    UserRegistrationMailer.with(user_registration:).requested
+  end
 end
--- test/mailers/user_registration_mailer_test.rb
+++ test/mailers/user_registration_mailer_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserRegistrationMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Rails.application.routes.url_helpers
+
+  test "requested" do
+    user_registration = user_registrations(:one)
+    mail = UserRegistrationMailer.with(user_registration:).requested
+    expected_url = new_user_registration_confirmation_url(user_registration, token: user_registration.token)
+
+    assert_equal "User registration has not yet been completed.", mail.subject
+    assert_equal [ user_registration.email ], mail.to
+    assert_equal [ "from@example.com" ], mail.from
+    assert_match expected_url, mail.body.encoded
+  end
+
+  private
+
+  def default_url_options = Rails.configuration.action_mailer.default_url_options
 end
