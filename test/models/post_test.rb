e9bb7287d4a9af689e4c4c3d686c7108f47b68a5
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-15 22:10:01 +0900

    add Post model test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "user must exist" do
+    post = Post.new(user: nil)
+    assert_not post.valid?
+    assert_includes post.errors[:user], "must exist"
+  end
+
+  test "contet must be present" do
+    post = Post.new(content: nil)
+    assert_not post.valid?
+    assert_includes post.errors[:content], "can't be blank"
   end
 end
