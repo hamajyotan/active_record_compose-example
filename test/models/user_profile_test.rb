bfb425437c99f5223f352eaf90c4c2652254dbd6
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-17 23:20:59 +0900

    add UserProfile model test

--- test/models/user_profile_test.rb
+++ test/models/user_profile_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 class UserProfileTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "valid" do
+    user = users(:three)
+    user_profile = UserProfile.new(user:, name: "Carol", lang: :en)
+    assert user_profile.valid?
+  end
+
+  test "Invalid if user is not set" do
+    user = users(:three)
+    user_profile = UserProfile.new(user:, name: "Carol", lang: :en)
+    assert user_profile.valid?
+
+    user_profile.user = nil
+    assert_not user_profile.valid?
+    assert_includes user_profile.errors[:user], "must exist"
+  end
+
+  test "Invalid if name is not set" do
+    user = users(:three)
+    user_profile = UserProfile.new(user:, name: "Carol", lang: :en)
+    assert user_profile.valid?
+
+    user_profile.name = nil
+    assert_not user_profile.valid?
+    assert_includes user_profile.errors[:name], "can't be blank"
+  end
+
+  test "Invalid if lang is not set" do
+    user = users(:three)
+    user_profile = UserProfile.new(user:, name: "Carol", lang: :en)
+    assert user_profile.valid?
+
+    user_profile.lang = nil
+    assert_not user_profile.valid?
+    assert_includes user_profile.errors[:lang], "can't be blank"
+    assert_includes user_profile.errors[:lang], "is not included in the list"
+  end
+
+  test "Invalid if lang is set unknown locale" do
+    user = users(:three)
+    user_profile = UserProfile.new(user:, name: "Carol", lang: :en)
+    assert user_profile.valid?
+
+    user_profile.lang = :fr
+    assert_not user_profile.valid?
+    assert_includes user_profile.errors[:lang], "is not included in the list"
   end
 end
