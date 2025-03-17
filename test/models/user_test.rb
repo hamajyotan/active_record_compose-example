e91f0f7ca59ac8ca08ccc19ef8396db43f3904c7
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-17 22:11:37 +0900

    add User model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,17 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
-
   test "valid" do
     user = users(:one)
     assert user.valid?
   end
 
-  test "#nate delegates to UserProfile" do
+  test "#name delegates to UserProfile" do
     user = users(:one)
     assert_equal user.name, "MyStringA"
+  end
+
+  test "If there is no UserCredential, #name returns a string indicating that the membership has been canceled" do
+    user = users(:resigned_user)
+    assert_equal user.name, "Resigned user"
+    I18n.with_locale(:ja) { assert_equal user.name, "退会済みユーザ" }
+    I18n.with_locale(:en) { assert_equal user.name, "Resigned user" }
   end
 end
