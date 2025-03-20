faf89d1ffae8b1319e8c35c946697ab057a4a1fc
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-03-20 16:59:47 +0900

    fix local assign name

--- test/models/user_registration_test.rb
+++ test/models/user_registration_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class UserRegistrationTest < ActiveSupport::TestCase
   test "email must be present" do
-    user_credential = UserCredential.new(email: nil)
-    assert_not user_credential.valid?
-    assert_includes user_credential.errors[:email], "can't be blank"
+    user_registration = UserCredential.new(email: nil)
+    assert_not user_registration.valid?
+    assert_includes user_registration.errors[:email], "can't be blank"
   end
 end
