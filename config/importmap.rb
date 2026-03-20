6e9fdda7ab65f6b2f1bdc27db4dcaf61f428db7f
Takashi SAKAGUCHI <hamajyotan@gmail.com>
2026-03-20 23:28:26 +0900

    importmap pin

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "bootstrap" # @5.3.8
-# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
