b68d00db5ade4071b7c4de2a27fb157b31d0d335
Takashi SAKAGUCHI <hamajyotan@gmail.com>
2026-03-21 07:14:32 +0000

    fix popper

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "bootstrap" # @5.3.8
 # pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
-pin "@popperjs/core", to: "popper.min.js" # @2.11.8
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
